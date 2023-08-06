5bf38e769586fad40b77a02f7cbcb4eb4b10efa4
maikimilk <maimu.ltd@gmail.com>
2023-08-06 18:25:20 +0900

    説明文の追加

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,16 @@
-import {graphql, Link, PageProps} from "gatsby";
+import {graphql, Link, PageProps } from "gatsby";
 import React from "react";
-import Layout from "../components/Layout.tsx";
+import Layout from "../components/Layout";
 import "../styles/About.css";
 import ExperienceList from "../components/ExperienceList";
+import {Image, Box, SimpleGrid} from '@chakra-ui/react'
 import { IconContext } from 'react-icons'
 import { FaTwitterSquare } from 'react-icons/fa'
 import { AiFillGithub } from 'react-icons/ai'
-import {fromatDate} from '../data'
 import index from './index';
 import NavigationButton from "../components/NavigationButton";
+import {StaticImage} from "gatsby-plugin-image";
+import {ExternalLinkIcon} from "@chakra-ui/icons";
 
 
 
@@ -35,7 +37,9 @@ const About: React.FC = () =>{
                 <h2>経歴</h2>
                 <ExperienceList experiences={experiences} />
                 <br/>
-            <h3>考え方</h3>
+            <h3>私について</h3>
+                <p>音楽、アニメ、映画、絵画など芸術や、バドミントン、ソフトテニスなどの運動が好き</p>
+                <p>APEX、Valorant やカービィなどのゲームもやります</p>
                 <br/>
             <h4>SNS</h4>
                 <IconContext.Provider value={{ size: '30px' }}>
@@ -48,6 +52,23 @@ const About: React.FC = () =>{
                         </Link>
                     </div>
                 </IconContext.Provider>
+                <br/>
+                <h5>使用可能技術</h5>
+                <br/>
+                <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={4}>
+                    <Box bg="gray.200" p={4}>
+                        <StaticImage src ='../images/product-a.png'/>
+                        <h2>プロフィールサイト</h2>
+                    </Box>
+                    <Box bg="gray.200" p={4}>
+                        <StaticImage src ='../images/product-b.png'/>
+                        <h3>論文通知BOT</h3>
+                    </Box>
+                    <Box bg="gray.200" p={4}>
+                        <StaticImage src ='../images/product-c.png'/>
+                        <h4>自律制御マシン</h4>
+                    </Box>
+                </SimpleGrid>
             </main>
         </Layout>
     )
